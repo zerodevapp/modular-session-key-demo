e5425fbba022bde05024edb78f1282e14210bc63
adnpark <aidenp.dev@gmail.com>
2024-02-19 00:30:10 +0900

    refactor: check to ensure key uniqueness

--- src/repository/PasskeyRepository.ts
+++ src/repository/PasskeyRepository.ts
@@ -1,60 +1,71 @@
-import { redisClient } from '../db';
-import db, { sql } from '../db';
-import { passkeyDomainObject } from '../objects/passkeyDomainObject';
+import { redisClient } from "../db"
+import db, { sql } from "../db"
+import { passkeyDomainObject } from "../objects/passkeyDomainObject"
 
 class PasskeyRepository {
-  static passkeyRepository: PasskeyRepository;
+    static passkeyRepository: PasskeyRepository
 
-  constructor() {
-    if (PasskeyRepository.passkeyRepository) {
-      return PasskeyRepository.passkeyRepository;
+    constructor() {
+        if (PasskeyRepository.passkeyRepository) {
+            return PasskeyRepository.passkeyRepository
+        }
+        PasskeyRepository.passkeyRepository = this
     }
-    PasskeyRepository.passkeyRepository = this;
-  }
 
-  async set(key: (string | number)[], value: any, expireIn?: number) {
-    const valueToStore = JSON.stringify(value);
-    if (expireIn) {
-      await redisClient.setex(JSON.stringify(key), expireIn, valueToStore);
-    } else {
-      await redisClient.set(JSON.stringify(key), valueToStore);
+    async set(key: (string | number)[], value: any, expireIn?: number) {
+        const serializedKey = JSON.stringify(key)
+        const valueToStore = JSON.stringify(value)
+
+        const exists = await redisClient.exists(serializedKey)
+        if (exists) {
+            throw new Error("Key already exists")
+        }
+
+        if (expireIn) {
+            await redisClient.setex(serializedKey, expireIn, valueToStore)
+        } else {
+            await redisClient.set(serializedKey, valueToStore)
+        }
     }
-  }
 
-  async get<T>(key: (string | number)[]): Promise<T | null> {
-    const value = await redisClient.get(JSON.stringify(key));
-    return value ? JSON.parse(value) as T : null;
-  }
+    async get<T>(key: (string | number)[]): Promise<T | null> {
+        const value = await redisClient.get(JSON.stringify(key))
+        return value ? (JSON.parse(value) as T) : null
+    }
 
-  async delete(key: (string | number)[]) {
-    await redisClient.del(JSON.stringify(key));
-  }
+    async delete(key: (string | number)[]) {
+        await redisClient.del(JSON.stringify(key))
+    }
 
-  async getPasskeyDomainByProjectId(projectId: string): Promise<string | null> {
-    const result = await (await db).maybeOne(sql.type(passkeyDomainObject)`
+    async getPasskeyDomainByProjectId(
+        projectId: string
+    ): Promise<string | null> {
+        const result = await (
+            await db
+        ).maybeOne(sql.type(passkeyDomainObject)`
       SELECT passkey_domain
       FROM project_passkey
       WHERE project_id = ${projectId}
-    `);
-    console.log({ result });
-    if (!result) {
-      return null;
+    `)
+        console.log({ result })
+        if (!result) {
+            return null
+        }
+        return getDomainFromUrl(result.passkeyDomain)
+        return result?.passkeyDomain || null
     }
-    return getDomainFromUrl(result.passkeyDomain);
-    return result?.passkeyDomain || null;
-  }
 }
 
-export default PasskeyRepository;
+export default PasskeyRepository
 
 function getDomainFromUrl(url: string): string {
-  try {
-      const hostname = new URL(url).hostname;
-      const parts = hostname.split('.');
-      const domain = parts.length > 2 ? parts[parts.length - 2] : parts[0];
-      return domain;
-  } catch (error) {
-      console.error("Invalid URL");
-      return "";
-  }
+    try {
+        const hostname = new URL(url).hostname
+        const parts = hostname.split(".")
+        const domain = parts.length > 2 ? parts[parts.length - 2] : parts[0]
+        return domain
+    } catch (error) {
+        console.error("Invalid URL")
+        return ""
+    }
 }
